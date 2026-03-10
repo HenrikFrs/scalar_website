f04e861934bf2f01796bb6497b8bf30351a41146
HenrikFrs <141763686+HenrikFrs@users.noreply.github.com>
2026-03-10 23:20:15 +0100

    build and changed deployment root path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/scalar_website', // wichtig für GitHub Pages!
   plugins: [react()],
 })
